dcb5e0d5d7d2fee0d10a2eb02c78933ee5198ffd
JuliaVal821 <julia.kolgatina@gmail.com>
2020-09-13 22:34:44 -0400

    Used operator if else

---
+++ Super Duper Easy.js
@@ -0,0 +1,9 @@
+https://www.codewars.com/kata/55a5bfaa756cfede78000026/train/javascript
+
+    function problem(x) {
+        if( typeof x === 'number') {
+            return x = x * 50 + 6;
+        } else {
+            return "Error";
+        }
+    }
